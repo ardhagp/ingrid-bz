aa0db92091ccf2fd6fde6b33c6a87b750a490505
ZENISLEV\titoa <tito.ardha@windowslive.com>
2026-08-08 18:21:37 +0700

    bugbash 20260808.2

---
+++ Ingrid-Bz/Services/KeySettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Services
+{
+    public class KeySettings
+    {
+        public required string SYNCFUSION { get; set; }
+        public required string SALT { get; set; }
+        public required string BETTERSTACK_LOG { get; set; }
+        public required string BETTERSTACK_HEARTBEATS { get; set; }
+        public required string LOCALDBPASSWORD { get; set; }
+        public required string CLOUDSTORAGE { get; set; }
+        public required string REPOPAGE { get; set; }
+    }
+}
