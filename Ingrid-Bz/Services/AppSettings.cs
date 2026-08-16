834826baae952c69bbb5fcaf545540750e02b09d
ZENISLEV\titoa <tito.ardha@windowslive.com>
2026-08-16 17:59:20 +0800

    bugbash 20260616.3

--- Ingrid-Bz/Services/AppSettings.cs
+++ Ingrid-Bz/Services/AppSettings.cs
@@ -3,9 +3,9 @@
     public class AppSettings
 {
         public string ApplicationName { get; set; } = "Ingrid BZ";
-        public int varMajor { get; set; } = 1;
-        public int varMinor { get; set; } = 0;
-        public int varBuild { get; set; } = 0;
-        public int varRevision { get; set; } = 0;
+        public int Major { get; set; } = 1;
+        public int Minor { get; set; } = 0;
+        public int Build { get; set; } = 0;
+        public int Revision { get; set; } = 0;
     }
 }
