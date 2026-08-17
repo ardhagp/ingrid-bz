820d755b43860d14fbd1562ba841315aff669eaf
ZENISLEV\titoa <tito.ardha@windowslive.com>
2026-08-18 00:37:52 +0800

    bugbash 20260818.1

--- Ingrid-Bz/Services/AppSettings.cs
+++ Ingrid-Bz/Services/AppSettings.cs
@@ -1,11 +1,26 @@
 ﻿namespace Services
 {
     public class AppSettings
-{
+    {
         public string ApplicationName { get; set; } = "Ingrid BZ";
         public int Major { get; set; } = 1;
         public int Minor { get; set; } = 0;
         public int Build { get; set; } = 0;
         public int Revision { get; set; } = 0;
+    }
+
+    public static class Function
+    {
+        public static string GetDevelopmentYear(int developmentyear)
+        {
+            if (developmentyear == System.DateTime.Now.Year)
+            {
+                return $"{System.DateTime.Now.Year}";
+            }
+            else
+            {
+                return $"{developmentyear} - {System.DateTime.Now.Year}";
+            }
+        }
     }
 }
